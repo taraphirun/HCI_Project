0c00d901447ab54c2984f1eaa2dfc0e7ecd20b97
KanghuiLiang <liangkanghuiapu@gmail.com>
2018-11-30 02:44:28 +0700

    searchbar

---
+++ findClass.php
@@ -0,0 +1,19 @@
+<?php
+include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<title>Find A Class</title>
+</head>
+<body>
+	<?php 
+	include 'search.php'
+	?>
+</div>
+</body>
+</html>
